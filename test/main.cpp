a9541ba8d6db6b9f0557119c8f6c73a3d7864867
Stephan Seitz <stephan.seitz@fau.de>
2021-01-01 17:24:27 +0100

    clang format

--- test/main.cpp
+++ test/main.cpp
@@ -5,21 +5,21 @@
  * Distributed under terms of the GPLv3 license.
  */
 
-#include <iostream>
-#include <cstdlib>
 #include "c10_c_api.h"
+#include <cstdlib>
+#include <iostream>
 
-auto main( int argc, char** argv ) -> int
-{
-    const int ONE_GPU = 1;
-    CUDACachingAllocator_init(ONE_GPU);
-    void* allocation = CUDACachingAllocator_raw_alloc(10);
-    CUDACachingAllocator_raw_delete(allocation);
-    std::cout << "Yeah! It worked on GPU." << std::endl;
+auto main(int argc, char **argv) -> int {
+  const int ONE_GPU = 1;
+  CUDACachingAllocator_init(ONE_GPU);
+  void *allocation = CUDACachingAllocator_raw_alloc(10);
+  CUDACachingAllocator_raw_delete(allocation);
+  std::cout << "Yeah! It worked on GPU." << std::endl;
 
-    void* cpuAllocation = CPUCachingAllocator_raw_alloc(10);
-    CPUCachingAllocator_raw_delete(cpuAllocation);
-    std::cout << "Yeah! It worked on CPU." << std::endl;
-    
-    return EXIT_SUCCESS;
+  void *cpuAllocation = CPUCachingAllocator_raw_alloc(10);
+  ((int *)cpuAllocation)[0] = 3;
+  CPUCachingAllocator_raw_delete(cpuAllocation);
+  std::cout << "Yeah! It worked on CPU." << std::endl;
+
+  return EXIT_SUCCESS;
 }
